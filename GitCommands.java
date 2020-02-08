bd045ddbf9b1b273a0f54390af2fbb78b34b348f
SreenivasSenisetty <sreenivas.rk93@gmail.com>
2020-02-08 15:29:52 +0530

    updated

--- GitCommands.java
+++ GitCommands.java
@@ -1,8 +1,9 @@
-Command
-mkdir
-touch
-vi
-rm -rf
-date
-cal
-soon
+git add *
+git commit -m ""
+git push az master
+git --global --list
+git remote -v
+git config --global user.name "username"
+git config --global user.mail "mailid"
+git init
+git status
