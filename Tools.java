6339b244cea5cde4223706f0a227371f08ab1de5
SreenivasSenisetty <sreenivas.rk93@gamil.com>
2020-09-30 11:20:06 +0530

    update code

--- Tools.java
+++ Tools.java
@@ -1,13 +1,13 @@
-OS	- Linux
-Version Control System - Git
-Scripting	- Shell
-Buildtool 	-Maven
-Testing 	-Sonarqube
-Deployment tool	- Apache Tomcat
-CI tool - Jenkins
-Containarization - Docker
+OS			- Linux
+Version Control System 	- Git
+Scripting		- Shell
+Buildtool 		- Maven
+Testing 		- Sonarqube
+Deployment tool		- Apache Tomcat
+CI tool 		- Jenkins
+Containarization 	- Docker
 Configuration management tool - Ansible
-Cloud	-AWS
-Artifactory repository - Nexus
-Ticketing tool - Jira
-Monitering tool - NewRelic
+Cloud			- AWS
+Artifactory repository 	- Nexus
+Ticketing tool 		- Jira
+Monitering tool 	- NewRelic
